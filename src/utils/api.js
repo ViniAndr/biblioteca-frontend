6c83ca2c0993b0915aa87cfecdb94b52a9f26a38
ViniAndr <fabioviniciusbarbosa@hotmail.com>
2026-03-24 10:53:04 -0300

    Correção do .env

--- src/utils/api.js
+++ src/utils/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 // Instância do axios com a URL base da API
 const api = axios.create({
-  baseURL: "http://localhost:3000/api",
+  baseURL: import.meta.env.VITE_API_URL || "http://localhost:3000/api",
 });
 
 api.interceptors.request.use(
@@ -16,7 +16,7 @@ api.interceptors.request.use(
   },
   (error) => {
     return Promise.reject(error);
-  }
+  },
 );
 
 export default api;
